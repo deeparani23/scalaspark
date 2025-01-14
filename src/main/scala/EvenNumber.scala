baeec9c3911e8d157439e9fa619c404090435798
deeparani23 <iamdeepa.2011@gmail>
2025-01-14 15:58:03 +0000

    This is Even and  Positive number Program

---
+++ src/main/scala/EvenNumber.scala
@@ -0,0 +1,13 @@
+object EvenNumber {
+  def main(args: Array[String]): Unit={
+    val a = 14
+    if (a > 0 && a% 2 == 0)
+    {
+      println("even and positive")
+    }
+    else
+    {
+      print("not both even and positive")
+    }
+  }
+}
