f419c4c8b8fbb58225b4043b287778067d16c1ac
deeparani23 <iamdeepa.2011@gmail>
2025-01-16 06:36:06 +0000

    This is Even and  Positive number Program

--- src/main/scala/EvenNumber.scala
+++ src/main/scala/EvenNumber.scala
@@ -1,13 +1,196 @@
 object EvenNumber {
-  def main(args: Array[String]): Unit={
-    val a = 14
-    if (a > 0 && a% 2 == 0)
-    {
-      println("even and positive")
-    }
-    else
-    {
-      print("not both even and positive")
-    }
-  }
-}
+  def main(args: Array[String]): Unit = {
+//1.
+    //    var a = 14
+    //    var c = a > 0 && a % 2 == 0
+    //    print(c)
+    //  }
+    //}
+    //2
+    //    var a = -15
+    //    var b = a < 10 || a > 20
+    //    print(b)
+    //  }
+    //}
+    //3.
+    //    var a = 27
+    //    var c = a%2!= 0 && a%3!= 0
+    //    print(c)
+    //  }
+    //}
+
+    //4
+    //var a = 18
+    //    var b = a%4== 0 || a%6 ==0
+    //    print(b)
+    //  }
+    //}
+
+    //5
+    //var age  = 20
+    //    var b = age >= 18 || age >=16
+    //    print(b)
+    //  }
+    //}
+
+    //6.
+    //var num  = 25
+    //    var num1 = (num >= 1 && num <= 10) || (num >= 20 && num <= 30)
+    //    print(num1)
+    //  }
+    //}
+    //7.
+    //    var a = -7
+    //    var b = a < 0 && a % 2 != 0
+    //    print(b)
+    //  }
+    //}
+
+    //8
+    //val age = 63
+    //
+    //if (age > 60)
+    //{
+    //  println("Person is eligible for Senior Citizen Discount.")
+    //}
+    //else if (age < 25)
+    //
+    //{
+    //  println("Person is eligible for Student Discount.")
+    //}
+    //else {
+    //  println("Person is not eligible for any discount.")
+    //}
+    //}
+    //}
+
+
+    //9
+    //    var a = 35
+    //    var b = a % 5 == 0 && a % 7 == 0
+    //    print(b)
+    //  }
+    //}
+
+    //10
+//    var a = -8
+//    var b = a >= 0 || a%2==0
+//    print(b)
+//  }
+//}
+
+//12.
+//val purchaseAmount = 120
+//
+//    if (purchaseAmount > 150) {
+//      println("Eligible for a discount.")
+//    }
+//    else if (purchaseAmount > 100) {
+//      println("Qualifies for free shipping.")
+//    }
+//    else {
+//      println("Not eligible for discount or free shipping.")
+//    }
+//  }
+//}
+
+//13.
+//var a= 24
+//    var b= a%3 ==0 || a%8 ==0
+//    print(b)
+//  }
+//}
+
+//14.
+//var a= -6
+//    var b=  (a <= 0 && a % 2 == 0)
+//    print(b)
+//  }
+//}
+
+//15.
+
+//    val age = 15
+//
+//    if (age < 13)
+//    {
+//      println("The person is a child.")
+//    }
+//    else if (age >= 13 && age <= 19)
+//    {
+//      println("The person is a teenager.")
+//    }
+//    else
+//    {
+//      println("The person is an adult.")
+//    }
+//  }
+
+
+//16.
+
+//    var a= 25
+//    var b = (a% 2==0 ||a%5 ==0)
+//    print(b)
+//  }
+//}
+
+//17.
+//val age = 70
+//
+//    if (age > 60 && age < 25)
+//    {
+//      println("Eligible for both Senior Citizen and Student Discount")
+//    }
+//    else {
+//      println("Not eligible for both discounts")
+//    }
+//  }
+//}
+
+//19.
+
+//    var num = 21
+//    var num2 = (num % 3 == 0 && num % 7 == 0)
+//    print(num2)
+//  }
+//}
+
+//20.
+//    var num = 45
+//    var num2 = (num % 5 == 0 || num % 9 == 0)
+//    print(num2)
+//  }
+//}
+
+//21
+
+//    var number = 15
+//    var number1 = (number % 2 != 0 && number % 4 != 0)
+//    print(number1)
+//  }
+//}
+
+//22
+//var number = 15
+//    var number2= (number % 3 == 0 && number % 5==0)
+//    print(number2)
+//  }
+//}
+//23.
+//var purchaseamount = 180
+//    var Loyaltycard = true
+//
+//    if (purchaseamount > 200 || Loyaltycard)
+//    {
+//      println("The customer is eligible for a discount or membership benefits")
+//    }
+//    else {
+//      println("The customer is not eligible for a discount or membership benefits")
+//    }
+//  }
+//}
+//
+
+//24.
+
